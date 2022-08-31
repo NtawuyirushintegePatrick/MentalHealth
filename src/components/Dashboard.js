1cd44825192d9137514a7a6656d7ec8b098c19cd
Ntawuyirushintege Patrick <ntawuyirushintegepatrick@gmail.com>
2022-08-31 08:07:52 +0200

    updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -112,6 +112,68 @@ const data = [
 ];
 
 
+const data1 = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: -1000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 500,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: -2000,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: -250,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const gradientOffset = () => {
+  const dataMax = Math.max(...data.map((i) => i.uv));
+  const dataMin = Math.min(...data.map((i) => i.uv));
+
+  if (dataMax <= 0) {
+    return 0;
+  }
+  if (dataMin >= 0) {
+    return 1;
+  }
+
+  return dataMax / (dataMax - dataMin);
+};
+
+const off = gradientOffset();
+
+
 
 
 
@@ -122,6 +184,10 @@ const Dashboardui = () => {
   class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/simple-area-chart-4ujxw';
 
+  }
+  class Example1 extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/area-chart-filled-by-sign-0h7rt';
+  
 
   }
 
@@ -826,12 +892,32 @@ const Dashboardui = () => {
                   </div> */}
                 </div>
                 <div className="col-lg-3">
-                  <article className="customers-wrapper" style={{color:'white'}}>
-                    <canvas
-                      id="customersChart"
-                      aria-label="Customers statistics"
-                      role="img"
-                    />
+                  <article className="customers-wrapper" style={{backgroundColor:'red'}}>
+                  <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data1={data1}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis data1Key="name" />
+          <YAxis />
+          <Tooltip />
+          <defs>
+            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+              <stop offset={off} stopColor="green" stopOpacity={1} />
+              <stop offset={off} stopColor="red" stopOpacity={1} />
+            </linearGradient>
+          </defs>
+          <Area type="monotone" data1Key="uv" stroke="#000" fill="url(#splitColor)" />
+        </AreaChart>
+      </ResponsiveContainer>
                   </article>
                   {/* <article className="white-block">
                     <div className="top-cat-title">
